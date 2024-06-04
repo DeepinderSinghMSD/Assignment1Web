61bee9c04aedc4364fbbf68d09cf58faa9a73456
Deepinder Singh <Deepindersingh4359@conestogac.on.ca>
2024-06-04 18:28:40 -0400

    Some changes in data file

--- src/data.js
+++ src/data.js
@@ -1,31 +1,31 @@
 const products = [
   {
     id: 1,
-    name: 'T-shirt',
+    name: 'T-shirt($15)',
     description: 'Trendy t-shirts featuring original artwork. crafted from supple, permeable cotton for comfortable wear all day. ideal for informal attire.',
     image: 'tShirt.jpeg',
   },
   {
     id: 2,
-    name: 'Shirt',
+    name: 'Shirt($20)',
     description: 'Shirts with versatility for formal and informal attire. Comfortable fit because to the superior fabric quality. Comes in a variety of sizes and colors.',
     image: 'shirt.jpeg',
   },
   {
     id: 3,
-    name: 'Jeans',
+    name: 'Jeans($30)',
     description: 'Stylish, long-lasting denim jeans with a slim fit. appropriate for any top, combining style and comfort. Boot-cut and vintage jeans also available',
     image: 'jeans.jpeg',
   },
   {
     id: 4,
-    name: 'Hoodies',
+    name: 'Hoodies($50)',
     description: 'Soft material hoodie that is both attractive and warm. has a roomy front pocket and a comfortable fit.',
     image: 'hoodies.jpeg',
   },
   {
     id: 5,
-    name: 'Shoes',
+    name: 'Shoes($80)',
     description: 'Everyday-use sneakers that are cozy and comfortable. Fits well with any outfit and offers outstanding support.',
     image: 'shoes.jpeg',
   },
